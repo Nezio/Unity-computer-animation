a087dd9adb2ab5be38697633f85d53b7b97e8b43
Nezio <nemanjaz1994@gmail.com>
2019-03-23 22:30:11 +0100

    Adding player movement scripts

---
+++ Animation/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        { // run
+            anim.CrossFade("Run", 0.5f);
+
+            anim.SetBool("Idle", false);
+            anim.SetBool("Run", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        { // stop runing
+            anim.SetBool("Run", false);
+            anim.SetBool("Idle", true);
+        }
+
+    }
+}
